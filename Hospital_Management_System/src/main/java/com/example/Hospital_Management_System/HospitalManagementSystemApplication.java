2e10beaa72d212d65f7fdc37d4fd31f9bc8a799c
Sandesh adhikari <sandesh12adk@gmail.com>
2025-05-30 18:58:32 +0545

    Changed @Bean to @OpenApiDefination to customize swagger documentation

--- Hospital_Management_System/src/main/java/com/example/Hospital_Management_System/HospitalManagementSystemApplication.java
+++ Hospital_Management_System/src/main/java/com/example/Hospital_Management_System/HospitalManagementSystemApplication.java
@@ -1,9 +1,27 @@
 package com.example.Hospital_Management_System;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Hospital Management System API Documentation",
+			 //   description = ""
+			//	version = "1.0.0",
+		   //		termsOfService = "https://example.com/terms",
+
+				contact = @Contact(
+						name = "Sandesh Adhikari",
+						email = "sandesh12adk@gmail.com",
+						url = "https://www.facebook.com/sandesh.adhikari.3323"
+				)
+		)
+)
 
 @SpringBootApplication
 @EnableJpaAuditing
