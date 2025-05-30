a58e9cbd0b6d846fb70f205aef056c029191e713
Sandesh adhikari <sandesh12adk@gmail.com>
2025-05-30 19:51:39 +0545

    Enhanced swagger doc

--- Hospital_Management_System/src/main/java/com/example/Hospital_Management_System/HospitalManagementSystemApplication.java
+++ Hospital_Management_System/src/main/java/com/example/Hospital_Management_System/HospitalManagementSystemApplication.java
@@ -1,8 +1,12 @@
 package com.example.Hospital_Management_System;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -11,16 +15,27 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @OpenAPIDefinition(
 		info = @Info(
 				title = "Hospital Management System API Documentation",
-			 //   description = ""
+			 //  description = ""
 			//	version = "1.0.0",
-		   //		termsOfService = "https://example.com/terms",
+		   //	termsOfService = "https://example.com/terms",
 
 				contact = @Contact(
 						name = "Sandesh Adhikari",
 						email = "sandesh12adk@gmail.com",
 						url = "https://www.facebook.com/sandesh.adhikari.3323"
 				)
-		)
+		),
+				security = @SecurityRequirement(
+						name = "bearerAuth"
+				)
+)
+
+@SecurityScheme(
+		name = "bearerAuth",
+		scheme = "bearer",
+		type = SecuritySchemeType.HTTP,
+		bearerFormat = "JWT",
+		in = SecuritySchemeIn.HEADER
 )
 
 @SpringBootApplication
